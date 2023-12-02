ec993523c1095d559b38e2556857a4988c858b00
Henrik wagner <henrikwagner2006@gmail.com>
2023-12-02 17:06:03 +0100

    Erster Commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class TodoController extends Controller
+{
+    public function store(Request $request)
+    {
+        $task = $request->input('task');
+        $taskpath = storage_path('app/tasks.json');
+        $task2 = json_decode(File::get($taskpath), true);
+        $task2[] = $task;
+        File::put($taskpath, json_encode($task2));
+
+        return redirect()->back();
+    }
+}
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<main>
+    <br><br>
+    <h1 class="title">TODO-List</h1>
+
+    <div class="container">
+        <form action="{{ route('store') }}" method="post">
+            @csrf
+            <div class="inputs">
+                <input class="task" type="text" name="task" placeholder="Add Task"> <br><br>
+                <input class="submit" name="submit" type="submit" value="Submit">
+                <div id="tasks" class="tasks">
+                        <p>-  <i id="icons" class="fa fa-trash"></i></p>
+                </div>
+            </div>
+        </form>
+    </div>
+</main>
+</body>
+</html>
+
